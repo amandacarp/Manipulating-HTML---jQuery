6ba8c1aa29ae8494883318f7b421812eb5901818
Amanda Carpentieri <carpentieri.a@gmail.com>
2020-11-13 16:46:09 -0800

    last button lists names with each click

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //1. Make a button appear on the page and make it alert a nice message
 let button = $('<button>CLICK ME TO ALERT A NICE MESSAGE</button>');
 $('body').append(button);
-button.click(function(){
+button.click(function () {
     alert("HELLO THERE");
 })
 
 //2. When second button is clicked - display an alert with the message in input field
-$('#secondButton').click(function(e){
+$('#secondButton').click(function (e) {
     e.preventDefault();
     let textValue = $("input[type=text]").val()
     alert($('input[type=text]').val());
@@ -14,26 +14,26 @@ $('#secondButton').click(function(e){
 
 //3a. hover over div to change color
 let divHover = $('#randomDiv')
-$(divHover).mouseover(function(){
+$(divHover).mouseover(function () {
     divHover.css('background-color', 'purple');
 })
 
 //3b. change back to originial color when mouse off
-$(divHover).mouseout(function(){
-    divHover.removeAttr('style');ß
+$(divHover).mouseout(function () {
+    divHover.removeAttr('style'); ß
 })
 
 //4. Click paragraph to change to random color
 let colors = ['red', 'blue', 'green', 'yellow', 'purple', 'pink', 'black'];
 
-function getRandomColor () {
+function getRandomColor() {
     let randomColor = colors[Math.floor(Math.random() * colors.length)];
     return randomColor;
 }
 let para = $('#para');
-$(para).click(function(){
-let randomColor = getRandomColor();
-para.css('color', randomColor);
+$(para).click(function () {
+    let randomColor = getRandomColor();
+    para.css('color', randomColor);
 })
 
 //5a. add button and empty div
@@ -43,19 +43,23 @@ let emptyDiv = $('<div></div>')
 $('body').append(emptyDiv);
 
 //5b. When the button is clicked, add a span that contains your name to the empty div
-$(buttonTwo).click(function(){
+$(buttonTwo).click(function () {
     let span = $('<span>Amanda</span>')
     $(emptyDiv).append(span);
 })
 
 //6. Create an array of friends, when button clicked add each friend as an li to ul
 let friends = ["Alex", "Alexandra", "Roger", "Adam", "Carolyn", "Matt", "Kim", "Amanda", "Adrianna", "Marissa", "Juliana"]
+let index = 0;
 
-$('#friendBtn').click(function(){
-    for (var i = 0; i < friends.length; i++){
+$('#friendBtn').click(function () {
+    if (index < friends.length) {
         let listItem = $('<li></li>');
         $('#list').append(listItem);
-        let listName = friends[i]
+        let listName = friends[index];
         $(listItem).append(listName);
+        index++;
+    } else{
+        index = 0;
     }
 })
